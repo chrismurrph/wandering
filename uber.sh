a6addae4e6ec638049ce3e7f77ff483a4b0cd8e6
Chris Murphy <chris@strandz.org>
2016-07-05 00:54:45 +1000

    Deployed this on Linode

---
+++ uber.sh
@@ -0,0 +1,10 @@
+mv ./project.clj.prod ./project.clj
+rm -rf ./resources/public/js
+rm -rf ./resources/public/wandering/js
+lein clean
+lein deps
+lein cljsbuild once prod
+lein uberjar
+mv ./project.clj ./project.clj.prod
+ln -s ./project.clj.dev ./project.clj
+mv ./target/*standalone.jar ./lib
